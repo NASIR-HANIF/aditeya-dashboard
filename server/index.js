742be40b9716cd67c57d09b62483839895b186d5
nasir hanif <nasir127sb@gmail.com>
2024-09-14 07:23:06 +0500

    server not found and hom route define

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-
-import dotenv from 'dotenv'
-dotenv.config()
-import connectDB from "./config/db.js";
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import routes from './routes/routes.js';
+
+dotenv.config();
 
 const app = express();
 
@@ -13,23 +13,29 @@ app.use(cors({
   origin: process.env.REACT_APP_API_URL
 }));
 
-
 // Middleware to parse form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Define routes
 app.use('/', routes);
 
-//PORT
+// Home route (optional)
+app.get('/', (req, res) => {
+  res.send('Welcome to the API!');
+});
+
+// 404 route for not found errors
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+// PORT
 const PORT = process.env.PORT || 8080;
 
-
-//Connect to the database before listening
+// Connect to the database before listening
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(
-      `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
-        .white
-    )
-  })
-})
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`);
+  });
+});
